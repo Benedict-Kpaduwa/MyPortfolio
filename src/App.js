708249ec4ae7b032bd76612cb41399cbb91d2b78
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-11-20 21:04:33 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GlobalStyle from "./globalStyles";
 import {ThemeProvider} from "styled-components";
 import {lightTheme} from "./components/Themes";
 import {DarkTheme} from "./components/Themes";
-import {Route, Switch} from "react-router";
+import {Route, Switch, useLocation} from "react-router";
 
 //components
 import Main from "./components/Main";
@@ -11,18 +11,27 @@ import BlogPage from "./components/BlogPage";
 import AboutPage from "./components/AboutPage";
 import WorkPage from "./components/WorkPage";
 import MySkillsPage from "./components/MySkillsPage";
+import { AnimatePresence } from "framer-motion";
+import SoundBar from "./subComponents/SoundBar";
 
 function App() {
+
+  const location = useLocation();
   return <>
     <GlobalStyle />
       <ThemeProvider theme={lightTheme}>
-        <Switch>
+        <SoundBar />
+
+        <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
           <Route exact path="/" component={Main} />
           <Route exact path="/about" component={AboutPage} />
           <Route exact path="/blog" component={BlogPage} />
           <Route exact path="/work" component={WorkPage} />
           <Route exact path="/skills" component={MySkillsPage} />
         </Switch>
+        </AnimatePresence>
+        
       </ThemeProvider>
     
   </>
