b327f4881ade84be04490db171568feeca17b0fb
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-11-20 22:21:57 +0100

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -5,7 +5,7 @@ export const Work = [
         description:"A movie database that allows users to view list of available movies to watch from, review them and rate it",
         tags:["react","styledComponents"],
         
-        demo:"benedictkrmdb.netlify.app",
+        demo:"https://benedictkrmdb.netlify.app",
         github:"https://github.com/Benedict-Kpaduwa/React-Movie-Database"
     },
     
